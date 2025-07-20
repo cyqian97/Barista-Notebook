747057c45c5cf3d0816ee7c9ee66f82c9db42f7b
Chengyuan Qian <cyqian@tamu.edu>
2025-07-19 21:25:24 -0500

    upgrade packages

--- flask-backend/init_db.py
+++ flask-backend/init_db.py
@@ -33,9 +33,9 @@ sample_beans = [
 with app.app_context():
     db.create_all()
 
-    # Clean up the coffee beans table
-    CoffeeBean.query.delete()
-    db.session.commit()
+    # # Clean up the coffee beans table
+    # CoffeeBean.query.delete()
+    # db.session.commit()
 
     # Add sample entries
     for bean_data in sample_beans:
